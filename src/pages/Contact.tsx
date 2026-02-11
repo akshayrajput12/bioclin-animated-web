2785a170ec38b5076df00b1b627d0cb307ce017e
bioclinpharm <107905516+bioclinpharm@users.noreply.github.com>
2026-02-11 05:13:21 -0500

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -105,6 +105,8 @@ const Contact = () => {
                     <h4 className="font-semibold">India</h4>
                     <p className="text-gray-600 dark:text-gray-300">
                       #11, Bhannergerghatta road, Bangalore, Karnataka, India Pincode-560076
+                      </p>
+                  <p className="text-gray-600 dark:text-gray-300">
                       Flat-14, Block-B,Maheshwari Towers, Prem Nagar Rd, Shyam Rao Nagar, Banjara Hills, Hyderabad Telangana 500034
                     </p>
                   </div>
